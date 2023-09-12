eb6d104810ade3f8ca931096dc6429dcdfc8a9e3
Cleberson Gomes <51521973+arktnld@users.noreply.github.com>
2023-09-12 00:55:59 -0300

    Update install.sh
    fix some problems

--- install.sh
+++ install.sh
@@ -40,11 +40,13 @@ github-cli
 exa
 "
 var_yum=""
-# eval "$package_manager" $packages
+
+# Update packages
+sudo pacman -Syu
 
 # Update keyring
-pacman-key --init
-pacman-key --populate archlinux
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
 
 # Check if running with root privileges
 if [[ $EUID -ne 0 ]]; then
@@ -53,37 +55,37 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install the primary key
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 
 # Install the Chaotic keyring and mirrorlist
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Append to /etc/pacman.conf
-echo '[chaotic-aur]' >> /etc/pacman.conf
-echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+sudo echo '[chaotic-aur]' >> /etc/pacman.conf
+sudo echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
 
 # Update Pacman
-pacman -Sy
+sudo pacman -Sy
 
 if [[ -f /etc/debian_version ]];then
-    apt-get update -y
-    apt-get upgrade -y
-    apt-get install -y $(echo "$var_debian" | tr '\n' ' ')
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install -y $(echo "$var_debian" | tr '\n' ' ')
     pip install trash-cli pyright
 elif [[ -f /etc/centos-release ]]; then
     echo test
 
 elif [[ -f /etc/arch-release ]]; then
-   pacman -S --noconfirm $(echo "$var_arch" | tr '\n' ' ')
+   sudo pacman -S --noconfirm $(echo "$var_arch" | tr '\n' ' ')
    echo "nameserver 1.1.1.1" >> /etc/resolv.conf # fix "gh auth login" error
 fi
 
 curl https://cht.sh/:cht.sh > ~/.local/bin/cht
 chmod +x ~/.local/bin/cht
 
-chsh -s /bin/zsh
-timedatectl set-timezone America/Sao_Paulo
+sudo chsh -s /bin/zsh
+sudo timedatectl set-timezone America/Sao_Paulo
 
 git clone --recurse-submodules https://github.com/arktnld/dotfiles
 
