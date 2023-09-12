a19645e3d7af31223e51b5bb229ae36b3fb04f43
Cleberson Gomes <51521973+arktnld@users.noreply.github.com>
2023-09-12 20:07:49 -0300

    Update install.sh
    Make the script more readable

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@
 # Created by: @arktnld
 #
 
+# Get username
+read -p "Enter your username: " username
+
 # Function Section
 
 # Define package lists for different distros
 declare -A packageLists
-packageLists["debian"]="neovim fzf nnn silversearcher-ag python-is-python3 python3-pip mlocate man-db man-pages tree bc patool zsh git gh exa"
+# packageLists["debian"]="neovim fzf nnn silversearcher-ag python-is-python3 python3-pip mlocate man-db man-pages tree bc patool zsh git gh exa"
 packageLists["arch"]="neovim fzf nnn the_silver_searcher trash-cli mlocate man-db man-pages tree bc patool zsh git github-cli exa paru"
 
 # Define distro-specific commands
@@ -17,28 +20,42 @@ declare -A updateCommands
 updateCommands["debian"]="apt update -y"
 updateCommands["arch"]="pacman -Syu --noconfirm"
 
+# Define distro-specific commands
+declare -A installCommands
+installCommands["debian"]="apt install -y"
+installCommands["arch"]="pacman -S --noconfirm"
+
+directories_to_move=(
+    "dotfiles/.config/zsh .config/ $username"
+    "dotfiles/.config/nvim .config/ $username"
+    "dotfiles/.config/less .config/ $username"
+    "dotfiles/.zshenv . $username"
+)
+
 # Define user-specific operations
-userOperations=("chsh -s /bin/zsh" "timedatectl set-timezone America/Sao_Paulo")
+userOperations=("chsh -s /bin/zsh")
 
 # Function to update packages
 update_packages() {
+    echo "Updating packages"
     local distro="$1"
     eval "${updateCommands[$distro]}"
 }
 
 # Function to install packages
 install_packages() {
-    local package_manager="$1"
-    local distro="$2"
-    eval "$package_manager install -y ${packageLists[$distro]}"
+    echo "install packages"
+    local distro="$1"
+    eval "${installCommands[$distro]}" "${packageLists[$distro]}"
 }
 
 # Function to perform user-specific operations
 perform_user_operations() {
-    local current_user
-    current_user=$(whoami)
+    echo "Perform user operations"
+    username=$1
     for operation in "${userOperations[@]}"; do
-        sudo -u "$current_user" $operation
+        sudo -u "$username" $operation
+
     done
 }
 
@@ -46,11 +63,14 @@ perform_user_operations() {
 move_directories() {
     local source_dir="$1"
     local destination_dir="$2"
-    sudo -u "$current_user" mv "$source_dir" "$destination_dir"
+    local username="$3"
+    echo mv "$source_dir" "$destination_dir"
+    sudo -u "$username" mv "$source_dir" "$destination_dir"
 }
 
 # Function to check for root privileges
 check_root_privileges() {
+    echo "Cheking root privilegies"
     if [[ $EUID -ne 0 ]]; then
         echo "This script must be run as root. Please use sudo or run as root."
         exit 1
@@ -59,6 +79,7 @@ check_root_privileges() {
 
 # Function to detect the distribution
 detect_distribution() {
+    # echo "Detecting distro name"
     if [[ -f /etc/debian_version ]]; then
         echo "debian"
     elif [[ -f /etc/arch-release ]]; then
@@ -68,50 +89,46 @@ detect_distribution() {
         exit 1
     fi
 }
-
-# Function to get the package manager based on the distribution
-get_package_manager() {
-    local distro="$1"
-    if [[ $distro == "debian" ]]; then
-        echo "apt"
-    elif [[ $distro == "arch" ]]; then
-        echo "pacman"
-    else
-        echo "Unsupported distribution."
-        exit 1
+update_timedate() {
+    is_wsl() {
+        grep -qi "Microsoft" /proc/version
+    }
+    if ! is_wsl; then
+      timedatectl set-timezone America/Sao_Paulo
     fi
 }
 
+# Function to get the package manager based on the distribution
 # Execution Section
 
 # Check for root privileges
-check_root_privileges
+ check_root_privileges
 
 # Detect the distribution
 distro=$(detect_distribution)
-
-
-# Get the package manager based on the distribution
-package_manager=$(get_package_manager "$distro")
+echo "distro: $distro"
 
 # Update and install packages
 update_packages "$distro"
-install_packages "$package_manager" "$distro"
+install_packages "$distro"
 
 # Perform user-specific operations
-perform_user_operations
+perform_user_operations "$username"
+
+# update timedate to Brazil
+update_timedate
 
 # Clone dotfiles repository as the regular user
-sudo -u "$current_user" git clone --recurse-submodules https://github.com/arktnld/dotfiles
+sudo -u "$username" git clone --recurse-submodules https://github.com/arktnld/dotfiles
 
-# Move user-specific configurations
-move_directories "dotfiles/.config/zsh" "~/.config/"
-move_directories "dotfiles/.config/nvim" "~/.config/"
-move_directories "dotfiles/.config/less" "~/.config/"
-move_directories "dotfiles/.zshenv" "~/"
+
+# Move user-specific configurations using a for loop
+for dir_info in "${directories_to_move[@]}"; do
+    eval move_directories $dir_info
+done
 
 # Create a directory as the regular user
-sudo -u "$current_user" mkdir -p "~/.local/share/zsh"
+sudo -u "$username" mkdir -p "~/.local/share/zsh"
 
 # Cleanup
 rm -rf dotfiles
